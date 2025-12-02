3c9cd16640728d875df402e4fb2bb880a68d507c
Iván <ivanquesadapalmero@gmail.com>
2025-12-02 11:25:21 +0100

    #1 - Crear proyecto Spring Boot base

---
+++ src/test/java/com/planazo/PlanazoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.planazo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanazoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
